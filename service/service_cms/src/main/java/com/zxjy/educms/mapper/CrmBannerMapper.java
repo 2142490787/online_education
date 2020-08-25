e9e3f245f186ecb204a7ec37575cde6095af7144
21424 <2142490787@qq.com>
2020-08-25 21:05:53 +0800

    完成

---
+++ service/service_cms/src/main/java/com/zxjy/educms/mapper/CrmBannerMapper.java
@@ -0,0 +1,17 @@
+package com.zxjy.educms.mapper;
+
+import com.zxjy.educms.entity.CrmBanner;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 首页banner表 Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-03-07
+ */
+public interface CrmBannerMapper extends BaseMapper<CrmBanner> {
+
+}
+
---
+++ service/service_edu/src/main/java/com/zxjy/eduservice/service/EduSubjectService.java
@@ -0,0 +1,25 @@
+package com.zxjy.eduservice.service;
+
+import com.zxjy.eduservice.entity.EduSubject;
+import com.zxjy.eduservice.entity.subject.OneSubject;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 课程科目 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-02-29
+ */
+public interface EduSubjectService extends IService<EduSubject> {
+
+    //添加课程分类
+    void saveSubject(MultipartFile file,EduSubjectService subjectService);
+
+    //课程分类列表（树形）
+    List<OneSubject> getAllOneTwoSubject();
+}
